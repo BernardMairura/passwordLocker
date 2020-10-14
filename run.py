b415d045d1ec6ea37a40a44814ef74a56a9ee81f
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-10-14 07:59:00 +0300

    import class to clear errors on my run.py file

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from details import User
 from details import Credentials
 import random
 
@@ -9,21 +10,22 @@ def create_user(fname, lname, email):
     new_user = User(fname, lname, email)
     return new_user
 
-def save_user(user):
+def save_user(User):
     """
     Function to save user
     """
-    user.save_user_details()
-
-
-def delete_user(user):
-    """
-    Function to delete a user
-    """
-    user.delete_user()
+    User.save_user_details()
 
 def display_user():
     """
     Function that returns saved users
     """
     return User.display_users()
+
+
+def delete_user(User):
+    """
+    Function to delete a user
+    """
+    User.delete_user()
+
