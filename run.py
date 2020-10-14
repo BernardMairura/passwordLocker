fb77e252334bbcff8388da9f7dbd31b8e892392a
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-10-14 08:01:13 +0300

    include create,save,display and delete credentials methods

--- run.py
+++ run.py
@@ -29,3 +29,34 @@ def delete_user(User):
     """
     User.delete_user()
 
+
+#credentials area
+
+def create_credential(username, password):
+    """
+    Function to create new user credentials
+    """
+    new_credential = Credentials(username, password)
+    return new_credential
+
+def save_credential(Credentials):
+    """
+    Function to save user credentials
+    """
+    Credentials.save_credential()
+
+def display_credential():
+    """
+    function that returns saved user credentials
+    """
+    return Credentials.display_credential()
+
+def delete_credential(Credentials):
+    """
+    Function to delete all users credentials
+    """
+    Credentials.delete_credential()
+
+
+
+
